a6f45f9cd7f158cab01f455a921541f50ac476d5
oussco <143568860+oussco@users.noreply.github.com>
2023-11-06 11:41:25 -0500

    Add files via upload
    lundi

---
+++ 42CURSUS/installette/README.md
@@ -0,0 +1,30 @@
+# Installette <img src="https://github.com/devicons/devicon/blob/master/icons/bash/bash-original.svg" title="Bash" alt="Bash Logo" width="50" height="60" align="right" />&nbsp; <img src="https://github.com/devicons/devicon/blob/master/icons/linux/linux-original.svg" title="Linux" alt="Linux Logo" width="50" height="60" align="right" />&nbsp;
+
+## About  
+Installette is a bash script designed to be a simplified and automated setup for the 42Schools' environment.   
+Currently supporting Linux distros that use the apt package manager and have bash.  
+
+### Installing and running installette:
+1- Clone this repository
+
+	git clone https://github.com/Kuninoto/installette  
+2- Navigate to _installette_
+
+	cd installette  
+3- Run _installette.sh_ with your desired options.
+
+	bash installette.sh -[option1] -[option2] [...]
+
+#### Available Options:
+`-v`, `--vim` - Installs Vim  
+`-h`, `--header` - Installs 42Header for Vim  
+`-n`, `--norminette` - Installs Norminette  
+`-f`, `--formatter` - Installs Norminette Formatter  
+`-u`, `--uninstall` - Uninstalls 42Header  
+#### Credits
+[42header.vim](https://github.com/gcamerli/42header)  
+[c_formatter_42](https://github.com/dawnbeen/c_formatter_42)   
+
+---
+Made by Nuno Carvalho (Kuninoto) | nnuno-ca@student.42porto.com  
+<div id="badge"> <a href="https://www.linkedin.com/in/nuno-carvalho-218822247"/> <img src="https://img.shields.io/badge/LinkedIn-blue?style=for-the-badge&logo=linkedin&logoColor=white" alt="LinkedIn Badge"/>&nbsp;
---
+++ 42CURSUS/libft.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ousmane <ousmane@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/21 19:36:00 by oba               #+#    #+#             */
+/*   Updated: 2023/11/04 14:14:09 by ousmane          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h> 
+
+int		ft_atoi(const char *str);
+int		ft_isalnum(int c);
+size_t	ft_strlen(const char *src);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memset(void *b, int c, size_t len);
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t count, size_t size);
+size_t	ft_strlcat(char *dst, const char *restrict src, size_t dstsize);
+char	*ft_strdup(char *src);
